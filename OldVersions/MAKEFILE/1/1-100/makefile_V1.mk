4a37323a25b1ed2a3d08c214bf083a84cfc45b12
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-08-26 18:15:12 -0700

    makefile_V1.mk
    An archive of the first version of the makefile for this project.

---
+++ OldVersions/MAKEFILE/1/1-100/makefile_V1.mk
@@ -0,0 +1,13 @@
+# Start of script
+# The Makefile for this project
+# Rename the secondary copying license
+# Since I don't know how to just rename a file, I will copy it and delete the original, that is as close as I can get with GNU Make right now
+copy /COPYINGL to /COPYING
+rm -f /COPYINGL
+echo "COPYING license file has been corrected."
+# This syntax isn't valid yet, I don't know how to write in GNU Make very well
+# File info
+# File type: Makefile (MAKEFILE, *.mk)
+# File version: 1 (Wednesday, August 18th 2021 at 6:41 pm)
+# Line count (including blank lines and compiler line): 14
+# End of script
